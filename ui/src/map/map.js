cffd651c69119de26d0242dd0e3a672bc6cde4d3
Askar Akhmerov <askar.akhmerov@smava.de>
2016-03-05 21:05:37 +0100

    customize icon

--- ui/src/map/map.js
+++ ui/src/map/map.js
@@ -62,8 +62,18 @@ export class Map {
 //      add guides
         for (let i = 0; i < this.markers.length; i++) {
             let marker = this.markers[i];
+            var guideIcon = {
+                path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+                fillColor: 'yellow',
+                fillOpacity: 0.8,
+                scale: 0.1,
+                strokeColor: 'gold',
+                strokeWeight: 14
+            };
+
             let markerPin = new google.maps.Marker({
                 map: this.map,
+                icon : guideIcon,
                 animation: google.maps.Animation.DROP,
                 position: new google.maps.LatLng(marker.position[0], marker.position[1])
             });
