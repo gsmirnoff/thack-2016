6e5fd8859f6d8eff65a008594ac069c546a60692
Greg Smirnoff <gregsm1988@gmail.com>
2016-03-05 19:02:07 +0100

    user model

---
+++ ui/src/models/user.js
@@ -0,0 +1,8 @@
+export class User {
+  constructor(o) {
+    this.name = o.name;
+    this.address = o.address;
+    this.email = o.email;
+    this.id = o.id;
+  }
+}
--- ui/src/register/register.js
+++ ui/src/register/register.js
@@ -1,13 +1,22 @@
-import {Component, View} from 'angular2/core';
+import {Component} from 'angular2/core';
+import {FormBuilder, Validators} from 'angular2/common';
+import {User} from '../models/user'
 
 @Component({
-  selector: 'registation'
-})
-
-@View({
+  selector: 'registation',
   templateUrl: 'register/register.html'
 })
 
 export class Register {
+  constructor(fb: FormBuilder) {
+    this.registrationForm = fb.group({
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      email: ['', Validators.required]
+    });
+  }
+  onSubmit() {
+    console.log('register', new User(this.registrationForm.value));
+  }
 
 }
