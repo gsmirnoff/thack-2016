71f41569c60dc506b64877d3dc31f2f8255b6088
Askar Akhmerov <askar.akhmerov@smava.de>
2016-03-05 15:40:48 +0100

    add endpoints to create and update users

--- rest-api/src/main/java/com/thack/localguides/controller/UserRestController.java
+++ rest-api/src/main/java/com/thack/localguides/controller/UserRestController.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -26,11 +23,13 @@ public class UserRestController {
     private UserRepository userRepository;
 
     /**
+     * This endpoint is here for debugging and testing purposes only,
+     * there is no plan so far to display all the users available in the system
      *
-     * @return
+     * @return - list of all users available in the system
      */
     @GET
-    @Path("guides")
+    @Path("all")
     @Produces({ MediaType.APPLICATION_JSON })
     public Iterable<User> getGuides() {
         return userRepository.findAll();
@@ -42,12 +41,45 @@ public class UserRestController {
      *  - guide see more information about specific users that have booked contact with him
      *  - tourist see more information about the guide near him
      *
-     * @return
+     * @return - specific user corresponding to provided ID
      */
     @GET
-    @Path("guide/{id}")
+    @Path("/{id}")
     @Produces({ MediaType.APPLICATION_JSON })
     public User getUser(@PathParam("id") String id) {
         return userRepository.findOne(id);
+    }
+
+    /**
+     * This method is handling both save new user entity and update existing user entity
+     * with new data set.
+     *
+     * Please note that update operation is performed based on id field in user entity
+     *
+     * curl -H "Content-Type: application/json" -X POST -d '{"firstName" : "JoeTest","lastName" : "BlackTest", "position" : [52.50023,13.40490]}' http://localhost:8080/api/users
+     * @param toSave
+     * @return
+     */
+    @POST
+    @Path("/")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Consumes({ MediaType.APPLICATION_JSON })
+    public User createUser(User toSave) {
+        return userRepository.save(toSave);
+    }
+
+    /**
+     * This endpoint is here for convenience only, implementation is same as createUser
+     *
+     * curl -H "Content-Type: application/json" -X PUT -d '{"id":"56daef6d54504486af708c7a","firstName":"JoeTest1","lastName":"BlackTest1","position":[52.50023,13.4049]}' http://localhost:8080/api/users
+     * @param toSave
+     * @return
+     */
+    @PUT
+    @Path("/")
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Consumes({ MediaType.APPLICATION_JSON })
+    public User updateUser(User toSave) {
+        return userRepository.save(toSave);
     }
 }
