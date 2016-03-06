fa80a1cb993d4184c5ddbe8719ef2c2ed884a808
Greg Smirnoff <gregsm1988@gmail.com>
2016-03-06 08:21:21 +0100

    interests

--- ui/src/register/register.js
+++ ui/src/register/register.js
@@ -18,6 +18,12 @@ export class Register {
     this.userService = userService;
     this.authenticationService = authenticationService;
     this.router = router;
+    this.interests = [
+      {value: 'interest 1', fg: 'formgroup', inputClass: 'input', textClass: 'text10'},
+      {value: 'interest 2', fg: 'formgroup1', inputClass: 'input1', textClass: 'text11'},
+      {value: 'interest 3', fg: 'formgroup2', inputClass: 'input2', textClass: 'text12'},
+      {value: 'interest 4', fg: 'formgroup3', inputClass: 'input3', textClass: 'text13'}
+    ];
     this.registrationForm = fb.group({
       name: ['', Validators.required],
       email: ['', Validators.required]
